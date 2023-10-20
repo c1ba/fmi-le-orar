55833dbbe057ff909a9842a1d7d7c7c15f16f624
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2023-10-20 08:41:51 +0300

    I hope this one fixes as well

--- src/utils/CourseUtils.ts
+++ src/utils/CourseUtils.ts
@@ -8,7 +8,7 @@ export const verifyIfCourseIsNow = (subject: any, nowDate: Date , beginningHour:
 export const courseIsInTheFuture = (subject: any, nowDate: Date, beginningHour: number, timeDuration: number) => 
 (!subject.onceEveryTwoWeeks || 
     (subject.onceEveryTwoWeeks && 
-        (Boolean(getWeekOfMonth(nowDate) % 2) === subject.isOdd))) && 
+        (Boolean(getWeekOfMonth(nowDate) % 2) === !subject.isOdd))) && 
         ((beginningHour + timeDuration) > nowDate.getHours());
 
 export const checkIfDuplicates = (coursesArray: {[key: string]: string | number | boolean}[], courseData: {[key: string]: string | number | boolean}) => !coursesArray.find((course) => course.name === courseData.name && course.type === courseData.type);
