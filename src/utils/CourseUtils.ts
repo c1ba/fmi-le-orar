9f7770044d75dd375bf8587206ec12f3024cfc85
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2023-10-20 08:37:41 +0300

    Possible fix of schedule

--- src/utils/CourseUtils.ts
+++ src/utils/CourseUtils.ts
@@ -1,12 +1,14 @@
+import { getWeekOfMonth } from "./TimeUtils";
+
 export const verifyIfCourseIsNow = (subject: any, nowDate: Date , beginningHour: number, timeDuration: number) => 
 (!subject.onceEveryTwoWeeks || 
-    (subject.onceEveryTwoWeeks && (!Boolean(nowDate.getDate() / 7 % 2) === subject.isOdd))) && 
+    (subject.onceEveryTwoWeeks && (Boolean(getWeekOfMonth(nowDate) % 2) === subject.isOdd))) && 
 (beginningHour <= nowDate.getHours() && beginningHour + timeDuration > nowDate.getHours())
 
 export const courseIsInTheFuture = (subject: any, nowDate: Date, beginningHour: number, timeDuration: number) => 
 (!subject.onceEveryTwoWeeks || 
     (subject.onceEveryTwoWeeks && 
-        (!Boolean(nowDate.getDate() / 7 % 2) === subject.isOdd))) && 
+        (Boolean(getWeekOfMonth(nowDate) % 2) === subject.isOdd))) && 
         ((beginningHour + timeDuration) > nowDate.getHours());
 
 export const checkIfDuplicates = (coursesArray: {[key: string]: string | number | boolean}[], courseData: {[key: string]: string | number | boolean}) => !coursesArray.find((course) => course.name === courseData.name && course.type === courseData.type);
