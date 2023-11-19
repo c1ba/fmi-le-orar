c243e7b63f3ea22297c76bd0a163a4d5d9bb5bdd
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2023-11-19 21:27:43 +0200

    Re-added the export

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -1,4 +1,4 @@
-const getWeekOfMonth = (date: Date) => {
+export const getWeekOfMonth = (date: Date) => {
     let adjustedDate = new Date(new Date().setDate(date.getDate() - date.getDay())).getDate();
     let prefixes = ['1', '2', '3', '4', '5', '6'];
     return (parseInt(prefixes[0 | Math.round(adjustedDate / 7)]));
