d5cbfd31b15a8f652523096259611f12751cfab3
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2023-11-19 21:26:29 +0200

    This surely I hope will fix it

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -1,5 +1,5 @@
-export const getWeekOfMonth = (date: Date) => {
-    let adjustedDate = date.getDate() + (6 - date.getDay());
+const getWeekOfMonth = (date: Date) => {
+    let adjustedDate = new Date(new Date().setDate(date.getDate() - date.getDay())).getDate();
     let prefixes = ['1', '2', '3', '4', '5', '6'];
-    return (parseInt(prefixes[0 | adjustedDate / 7]));
+    return (parseInt(prefixes[0 | Math.round(adjustedDate / 7)]));
 }
